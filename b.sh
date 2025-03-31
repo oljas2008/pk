ce769f8526900f6f2e3a658567bf20aa6de73296
oljas2008 <86162005+oljas2008@users.noreply.github.com>
2025-03-31 18:21:08 +0000

    Build script: some edits

--- b.sh
+++ b.sh
@@ -164,7 +164,7 @@ else
 
     curl -s -X POST "https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@Ximipurekernel" \
     -F document=@"./PureLine-munch-$MAGIC_BUILD_DATE.zip" \
-    -F caption="PureLine (trying to add susfs) branch: ${BRANCH}" \
+    -F caption="PureLine (Just testing) branch: ${BRANCH}" \
     -F message_thread_id="2"
    
 BUILD=$((BUILD + 1))
